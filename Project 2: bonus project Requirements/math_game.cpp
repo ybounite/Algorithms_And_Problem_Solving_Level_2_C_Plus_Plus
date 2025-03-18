db94a8704f7a8b051c7c9efcde705b1c890bab23
YOUSSEF-code <136598297+YOSSEF-cod@users.noreply.github.com>
2025-03-18 10:34:51 +0000

    updat

--- Project 2: bonus project Requirements/math_game.cpp
+++ Project 2: bonus project Requirements/math_game.cpp
@@ -1,126 +1,244 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// enum	enQuizzLevel { Easy = 1 , Mad = 2, Hard = 3, Mix = 4};
+// enum	enOperations { Add = 1, Sub = 2, Mul = 3, Div = 4, Mixe = 5};
+
+// struct	stQuizz
+// {
+// 	enQuizzLevel	QuestionLevel;
+// 	enOperations	Operation;
+// 	short			GeneratNumber1;
+// 	short			GeneratNumber2;
+// };
+
+// string tabs(short HowManyTabs)
+// {
+// 	string tabs;
+// 	for (short counter = 0; counter < HowManyTabs; counter++)
+// 		tabs += "\t";
+// 	return tabs;
+// }
+
+// short	ReadHowManyQuestions(string Message)
+// {
+// 	short nbr;
+// 	do
+// 	{
+// 		cout << Message << endl;
+// 		cin >> nbr;
+// 	} while (nbr < 0 && nbr < 11);
+// 	return (nbr);
+// }
+
+// enQuizzLevel	ReadQuestionsLevel()
+// {
+// 	short QusetionsLevel;
+// 	do
+// 	{
+// 		cout << "Enter Questions Level" << endl;
+// 		cout << "Easy [1], Med [2], Hard [3], Mix [4] ?" << endl;
+// 		cout << "Enter your choise : ";
+// 		cin >> QusetionsLevel;
+// 	} while (QusetionsLevel < 0 || QusetionsLevel > 4);
+// 	return ((enQuizzLevel)QusetionsLevel);
+// }
+
+// enOperations	ReadOperation()
+// {
+// 	short Operation;
+// 	do 
+// 	{
+// 		cout << tabs(2) << "Enter OPeration Type ? " << endl;
+// 		cout <<  "Add [1], Sub [2] , Mul [3], Mix [5] ?" << endl;
+// 		cout << "Enter your choise : ";
+// 		cin >> Operation;
+// 	}while (Operation < 0 || Operation > 5);
+// 	return ((enOperations)Operation);
+// }
+
+// void	ResetSecreen()
+// {
+// 	system("clr");
+// 	system("color 0F");
+// }
+
+
+
+// short RandomNumber(short To, short From)
+// {
+// 	return (rand() % (From - To - 1) + To);
+// }
+
+// // short	AddOperation(short Number1, short Number2)
+// // {
+
+// // }
+// // short	SubtractionOperation()
+// // {
+
+// // }
+// // short	multiplyOperation()
+// // {
+
+// // }
+// // short	multiplyOpearation()
+// // {
+
+// // }
+// // short	MixOpearation()
+// // {
+
+// // }
+
+// stQuizz	PlayGameMath(short HowManyQuestions)
+// {
+// 	stQuizz QuizzInfo;
+
+// 	for (short Count = 1; Count <= HowManyQuestions; Count++)
+// 	{
+// 		cout << "Question [" << Count << "/" << HowManyQuestions << "]" << endl;
+// 		QuizzInfo.QuestionLevel = ReadQuestionsLevel();
+// 		QuizzInfo.Operation = ReadOperation();
+// 		QuizzInfo.GeneratNumber1 = RandomNumber(1, 10);
+// 		QuizzInfo.GeneratNumber2 = RandomNumber(1, 10);
+// 	}
+// 	return QuizzInfo;
+// }
+
+// void	StartGameMath()
+// {
+// 	char PlayAgain = 'y';
+
+// 	do
+// 	{
+// 		ResetSecreen();
+// 		stQuizz Quizz = PlayGameMath(ReadHowManyQuestions("How Many Questiond do you want to answer ?"));
+		
+// 		cout << tabs(0) << "Do you want to play again Y/N" << endl;
+// 		cin >> PlayAgain;
+// 	} while (PlayAgain == 'y' || PlayAgain == 'Y');
+// }
+
+// int	main()
+// {
+// 	srand((unsigned)time(NULL));
+
+// 	StartGameMath();
+// }
+#include <iostream>
 using namespace std;
 
-enum	enQuizzLevel { Easy = 1 , Mad = 2, Hard = 3, Mix = 4};
-enum	enOperations { Add = 1, Sub = 2, Mul = 3, Div = 4, Mixe = 5};
+enum enOperationType { Add = 1, sub = 2, Mul = 3, Div  = 4, MIX = 5 };
+enum enQuestionLevel { Easy = 1, Med = 2, Hard = 3, Mix = 4};
 
-struct	stQuizz
+struct stQuizz
 {
-	enQuizzLevel	QuestionLevel;
-	enOperations	Operation;
-	short			GeneratNumber1;
-	short			GeneratNumber2;
+	enQuestionLevel LeveQuestion;
+	enOperationType OperationType;
+	short			Number1;
+	short			Number2;
+	short			Answer;
+	short			Result;
+	char			CharOperationType;
 };
 
-string tabs(short HowManyTabs)
+short	ReadHowManyQuestions()
 {
-	string tabs;
-	for (short counter = 0; counter < HowManyTabs; counter++)
-		tabs += "\t";
-	return tabs;
-}
-
-short	ReadHowManyQuestions(string Message)
-{
-	short nbr;
+	short NumberQuestion;
 	do
 	{
-		cout << Message << endl;
-		cin >> nbr;
-	} while (nbr < 0 && nbr < 11);
-	return (nbr);
+		cout << "How Many Questions do you want to answer ?" << endl;
+		cin >> NumberQuestion;
+	} while (NumberQuestion < 0 || NumberQuestion > 10);
+	return NumberQuestion;
 }
 
-enQuizzLevel	ReadQuestionsLevel()
+enQuestionLevel	ReadQuestionLevel()
 {
-	short QusetionsLevel;
+	short QuestionLevel;
 	do
 	{
-		cout << "Enter Questions Level" << endl;
-		cout << "Easy [1], Med [2], Hard [3], Mix [4] ?" << endl;
-		cout << "Enter your choise : ";
-		cin >> QusetionsLevel;
-	} while (QusetionsLevel < 0 || QusetionsLevel > 4);
-	return ((enQuizzLevel)QusetionsLevel);
+		cout << "Enter Question Level Easy : [1], Med : [2], Hard : [3],  Mix : [4] ?" << endl;
+		cin >> QuestionLevel;
+	} while (QuestionLevel < 0 || QuestionLevel > 4);
+	return (enQuestionLevel)QuestionLevel;
 }
 
-enOperations	ReadOperation()
+enOperationType	ReadOperationType()
 {
-	short Operation;
-	do 
+	short	OperationType;
+	do
 	{
-		cout << tabs(2) << "Enter OPeration Type ? " << endl;
-		cout <<  "Add [1], Sub [2] , Mul [3], Mix [5] ?" << endl;
-		cout << "Enter your choise : ";
-		cin >> Operation;
-	}while (Operation < 0 || Operation > 5);
-	return ((enOperations)Operation);
+		cout << "Enter Operation Type ?" << endl;
+		cout << "Add : [1], Sub : [2], Mul [3], Div : [4], MIX : [5] ?" << endl;
+		cin >> OperationType;
+	} while (OperationType < 0 || OperationType > 5);
+	return (enOperationType)OperationType;
 }
 
-void	ResetSecreen()
+short	RandomNumber(short to, short from)
 {
-	system("clr");
-	system("color 0F");
+	return (rand() % (from - to + 1) + to);
 }
 
+// short OperationAdd(short Number1, short Number2)
+// {
+	
+// }
 
-
-short RandomNumber(short To, short From)
+void	HeaderOperation(stQuizz Quizz)
 {
-	return (rand() % (From - To - 1) + To);
+	cout << "  " << Quizz.Number1 << endl;
+	cout << "  " << Quizz.Number2 << "  " << Quizz.CharOperationType;
+	cout << endl << "------------------------" << endl;
+	//cout << "  " << Quizz.Result;
 }
 
-// short	AddOperation(short Number1, short Number2)
-// {
-
-// }
-// short	SubtractionOperation()
-// {
-
-// }
-// short	multiplyOperation()
-// {
-
-// }
-// short	MixOpearation()
-// {
-
-// }
+short	ReadTheAnswer()
+{
+	short Answer;
+	cin >> Answer;
+	return Answer;
+}
 
 stQuizz	PlayGameMath(short HowManyQuestions)
 {
-	stQuizz QuizzInfo;
-
-	for (short Count = 1; Count <= HowManyQuestions; Count++)
+	stQuizz Quizz;
+	Quizz.LeveQuestion = ReadQuestionLevel();
+	Quizz.OperationType = ReadOperationType();
+	for (int counter = 0; counter < HowManyQuestions; counter++)
 	{
-		cout << "Question [" << Count << "/" << HowManyQuestions << "]" << endl;
-		QuizzInfo.QuestionLevel = ReadQuestionsLevel();
-		QuizzInfo.Operation = ReadOperation();
-		QuizzInfo.GeneratNumber1 = RandomNumber(1, 10);
-		QuizzInfo.GeneratNumber2 = RandomNumber(1, 10);
+		cout << "Question [" << counter + 1 << "]" << endl;
+		Quizz.Number1 = RandomNumber(1, 10);
+		Quizz.Number2 = RandomNumber(1, 10);
+		HeaderOperation(Quizz);
+		Quizz.Answer = ReadTheAnswer();
 	}
-	return QuizzInfo;
+	return Quizz;
 }
 
-void	StartGameMath()
+void	RestartScren()
 {
-	char PlayAgain = 'y';
+	system("color 0F");
+}
 
+void	StartGame()
+{
+	stQuizz	Quizz;
+	char PlayAgaine = 'y';
 	do
 	{
-		ResetSecreen();
-		stQuizz Quizz = PlayGameMath(ReadHowManyQuestions("How Many Questiond do you want to answer ?"));
-
-		cout << tabs(0) << "Do you want to play again Y/N" << endl;
-		cin >> PlayAgain;
-	} while (PlayAgain == 'y' || PlayAgain == 'Y');
-	
+		RestartScren();
+		Quizz = PlayGameMath(ReadHowManyQuestions());
+		cout << "Do you Want to Play Againe Y/N?";
+		cin >> PlayAgaine;
+	} while (PlayAgaine == 'Y' || PlayAgaine == 'y');
 }
-
-int	main()
+int main()
 {
 	srand((unsigned)time(NULL));
-
-	StartGameMath();
+	StartGame();
 }
+
